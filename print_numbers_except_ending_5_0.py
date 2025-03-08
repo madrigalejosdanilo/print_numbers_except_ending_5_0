f2d85cef055ce860d2194e3d3d10ec8870885b21
Danilo Madrigalejos <danilo.madrigalejos@vipre.com>
2025-03-08 17:27:35 +0800

    print output

--- print_numbers_except_ending_5_0.py
+++ print_numbers_except_ending_5_0.py
@@ -1,7 +1,8 @@
 # print all number 0 - 100
 
 for number in range(0, 101):
-    print(number)
 
-# check if ending with 5 or 0
-    # print if true
+    # check if ending with 5 or 0
+    if (number % 5) != 0:
+        # print if true
+        print(number)
